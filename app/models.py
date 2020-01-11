c732c067e4d2b76aae3219f38e0e56e4ecae1898
Denisa Maria <36793677+spopawdodo@users.noreply.github.com>
2020-01-11 13:50:45 +0200

    modified cart model

--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@ from django.db import models
 
 User = get_user_model()
 
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     birthday = models.DateField(blank=True, null=True)
     address = models.CharField(max_length=30, blank=True, null=True)
     credit = models.FloatField(default=0)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
@@ -23,7 +25,7 @@ class Article(models.Model):
 
 
 class Cart(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     articles = models.ManyToManyField(Article, related_name='carts', blank=True)
     sum = models.FloatField()
 
